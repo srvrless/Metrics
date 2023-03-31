f68b873bdd6b875cabf266a7c24871ea57c89699
DamnGachi <maijor18@mail.ru>
2023-04-01 00:02:44 +0300

    init migrations and add new models

---
+++ server/internal/db/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Boolean, Column, Float, Integer, String, DateTime, func
+from internal.utils.base import Base
+
+
+class Purchases(Base):
+    __tablename__ = 'purchases'
+    id = Column(Integer, primary_key=True)
+    purchase_name = Column(String, nullable=False, unique=False)
+    lot_name = Column(String, nullable=False, unique=False)
+    price = Column(Float, nullable=False, unique=False)
+    customer_inn = Column(Integer, nullable=False, unique=True)
+    customer_name = Column(String, nullable=False, unique=False)
+    delivery_region = Column(String, nullable=False, unique=False)
+    publish_date = Column(DateTime, default=func.now())
+    contract_category = Column(String, nullable=False, unique=False)
+
+
+class Participants(Base):
+    __tablename__ = 'participants'
+    id = Column(Integer, primary_key=True)
+    supplier_inn = Column(Integer, nullable=False, unique=True)
+    is_winner = Column(Boolean, default=False)
+
+
+class Contracts(Base):
+    __tablename__ = 'contracts'
+    id = Column(Integer, primary_key=True)
+    contract_reg_number = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+    contract_conclusion_date = Column(DateTime, nullable=False)
+
+
+class Companies(Base):
+    __tablename__ = 'companies'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    supplier_inn = Column(Integer, nullable=False, unique=True)
+    supplier_kpp = Column(String, nullable=False, unique=True)
+    okved = Column(String, nullable=False, unique=True)
+    status = Column(Boolean, default=True)
+    count_managers = Column(Integer, nullable=False)
