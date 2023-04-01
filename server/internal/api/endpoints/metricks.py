8166f76f8e2c5307fa8be597605d508999db32c9
Ilya Alexseevich <58089493+Eliac11@users.noreply.github.com>
2023-04-01 15:45:21 +0300

    revert metrics

--- server/internal/api/endpoints/metricks.py
+++ server/internal/api/endpoints/metricks.py
@@ -8,22 +8,20 @@ from internal.db.database import get_session
 from internal.db.models import Purchases, Contracts
 from fastapi.responses import StreamingResponse
 
-from internal.crud.write_data_csv import read_csv, write_data, insert_data
+from internal.crud.write_data_csv import read_csv, write_data,insert_data
 
 import json
 router = APIRouter()
 
-
 @router.post("/insert-csv-data")
-async def insert_csv_data(file: UploadFile = File(...)):
+async def insert_csv_data(file: UploadFile=File(...)):
     await insert_data(file)
     return {"message": "Data inserted successfully"}
-
 
 @router.post('/upload-csv')
 async def upload_csv(file: UploadFile = File(...), session: AsyncSession = Depends(get_session)):
     df = await read_csv(file)
-    await write_data(df, session)
+    await write_data(df,session)
     return {'status': 'ok'}
 
 
@@ -71,8 +69,32 @@ async def get_contracts_graph(session: AsyncSession = Depends(get_session)):
 
 @router.get("/get_prices_contracts")
 async def get_prices_contacts(session: AsyncSession = Depends(get_session)):
-    stmt = select(Purchases.price, Purchases.publish_date,
-                  Purchases.delivery_region)
+    stmt = select(Purchases.price, Purchases.publish_date, Purchases.delivery_region)
+    result = await session.execute(stmt)
+
+    prices = [(row[0],str(row[1]),row[2]) for row in result]
+    prices = sorted(prices, key=lambda x: x[1])
+
+    result = {}
+
+    for price in prices:
+        key = price[1].split(' ')[0]
+        region = price[2]
+        if region not in result:
+            result[region] = {}
+        if key not in result[region]:
+            result[region][key] = 0
+
+        result[region][key] += int(price[0])
+
+    f = open("tests_prices.json","w",encoding="utf-8")
+    json.dump(result,f,indent=3)
+    f.close()
+    return result
+
+@router.get("/get_prices_summaryRegion")
+async def get_prices_summaryRegion(session: AsyncSession = Depends(get_session)):
+    stmt = select(Purchases.price, Purchases.publish_date, Purchases.delivery_region)
     result = await session.execute(stmt)
 
     prices = [(row[0], str(row[1]), row[2]) for row in result]
@@ -81,14 +103,10 @@ async def get_prices_contacts(session: AsyncSession = Depends(get_session)):
     result = {}
 
     for price in prices:
-        key = price[1].split(' ')[0]
+        region = price[2]
+        if region not in result:
+            result[region] = 0
 
-        if key not in result:
-            result[key] = 0
-        result[key] += int(price[0])
-        print(price[2])
+        result[region] += int(price[0])
 
-    f = open("tests_prices.json", "w", encoding="utf-8")
-    json.dump(result, f, indent=3)
-    f.close()
     return result
